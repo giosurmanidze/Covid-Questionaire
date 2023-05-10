b19ff3a75a5c932eebb2633fa91e3690fcb9aac2
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-05-10 21:37:25 +0400

    style/tips page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../pages/Home.vue'
 import PrivateInfo from '../pages/PrivateInfo.vue'
 import CovidCondition from '../pages/CovidCondition.vue'
 import Vaccination from '../pages/Vaccination.vue'
+import Tips from '../pages/Tips.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +28,11 @@ const router = createRouter({
       name: 'vaccination',
       component: Vaccination
     },
+    {
+      path: '/tips',
+      name: 'tips',
+      component: Tips
+    }
   ]
 })
 
