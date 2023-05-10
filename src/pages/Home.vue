08368d1263bd0b88c44e97c2e574dd72b0c99f7b
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-05-10 20:38:58 +0400

    style/home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-screen bg-[#EAEAEA] flex flex-col items-center justify-center gap-16 pb-16">
+    <img src="../assets/redbbery_logo.svg" />
+    <RouterLink to="private-info" class="text-[30px] flex flex-col items-center"
+      ><strong>კითხვარის</strong>
+      <strong>დაწყება</strong>
+    </RouterLink>
+  </div>
+</template>
+
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+
+export default {}
+</script>
