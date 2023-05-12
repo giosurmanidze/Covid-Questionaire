48391b81b1e99ea7935c699c3bee208b6ca16631
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-05-12 12:45:36 +0400

    feat/validation and store data for private info page

---
+++ src/pages/ErrorScreen.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center justify-center h-screen">
+      <div class="bg-red-500 text-white px-8 py-4 rounded-lg shadow-lg">
+        <h1 class="text-2xl font-bold">Access denied!</h1>
+        <p class="text-lg">Validation failed.</p>
+      </div>
+    </div>
+  </template>
+  
